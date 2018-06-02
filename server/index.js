84d5ff3b0baee46aa517e2479c597b46daa4a677
Anoop Sownderraj <asownder95@gmail.com>
2018-06-01 19:36:26 -0700

    Test pull request - overviews.

--- server/index.js
+++ server/index.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3003;
 
-app.use(express.static(path.join(__dirname, '../public/')));
+app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/', (req, res) => {
 
-app.get();
+});
 
 
 app.listen(port, () => console.log(`Overview module listening on port ${port}`));
