828e481f0d811023238e2e386c97b3adabc8cb25
Anoop Sownderraj <asownder95@gmail.com>
2018-06-06 17:22:19 -0700

    Rendering restaurant name and ReviewsOverview component with example data.

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import ReviewsOverview from './ReviewsOverview.jsx';
 
 const Overview = (props) => (
-  <div>
-    This is a React Component!
+  <div className="card border-top-0 border-left-0 border-right-0 rounded-0">
+    <div className="card-body">
+      <h1 className="overview-title">Restaurant Name</h1>
+      <div><ReviewsOverview /></div>
+    </div>
+    
   </div>
 );
 
---
+++ client/src/components/ReviewsOverview.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ReviewsOverview = (props) => (
+  <div id="reviewsOverviewComponent">
+    <div className="avgReviewContainer">
+      <img src="./images/star-rating.png" width="70" height="30"></img>
+      <span className="avgReview">4.5</span>
+    </div>
+    <div className="totalReviewsContainer">
+      <span className="fa fa-comments"></span>
+      <span className="totalReviews">333 reviews</span>
+    </div>
+    <div className="priceRangeContainer">
+      <span className="fa fa-money"></span>
+      <span className="priceRange">$30 and under</span>
+    </div>
+    <div className="cuisineTypeContainer">
+      <span className="fa fa-cutlery test"></span>
+      <span className="cuisineType">Seafood</span>
+    </div>
+  </div>
+);
+
+export default ReviewsOverview;
