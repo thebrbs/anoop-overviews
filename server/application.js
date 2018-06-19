3a7cda9d8b4a496dc8bf1b029e134834ba468305
Anoop Sownderraj <asownder95@gmail.com>
2018-06-19 11:28:44 -0700

    Fixed individual microservice rendering.

--- server/application.js
+++ server/application.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('../database/index');
 
 const app = express();
 
-app.use(bodyParser.json());
-
+app.use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public/index.html')));
 app.use('/overviewsBundle.js', express.static(path.join(__dirname, '../public/dist/bundle.js')));
 app.use('/images/star-rating.png', express.static(path.join(__dirname, '../public/images/star-rating.png')));
 
-app.get('/restaurant/:restaurantId/overview', (req, res) => {
+app.get('/overviews/restaurant/:restaurantId/overview', (req, res) => {
   db.retrieve(req.params.restaurantId, (err, results) => {
     if (err && err.message.includes('Cast to number failed for value "NaN"')) {
       res.status(400).json('Bad request');
