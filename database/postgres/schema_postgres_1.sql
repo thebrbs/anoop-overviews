7cada5b12eb8fb91252ce6bf2ef4a76ff7553460
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-22 11:23:23 -0700

    Create database and tables for overviews postgres database, version 1.

--- database/postgres/schema_postgres_1.sql
+++ database/postgres/schema_postgres_1.sql
@@ -1,15 +1,17 @@
 DROP DATABASE IF EXISTS overviews;
 CREATE DATABASE overviews;
 
-\c overviews;
+\C overviews;
 
 CREATE TABLE IF NOT EXISTS overviews (
   rest_id serial PRIMARY KEY,
   rest_name  VARCHAR(100),
   price_range VARCHAR(100),
-  description VARCHAR(600),
+  description VARCHAR(900),
   dining_style VARCHAR(100),
   cuisine VARCHAR(30),
+  tot_reviews INTEGER,
+  avg_rating INTEGER,
   hours_of_operation_brkfst VARCHAR(100),
   hours_of_operation_lunch VARCHAR(100),
   hours_of_operation_dinner VARCHAR(100),
@@ -22,8 +24,8 @@ CREATE TABLE IF NOT EXISTS overviews (
   location_lng VARCHAR(30),
   neighborhood VARCHAR(100),
   cross_street VARCHAR(100),
-  parking_details VARCHAR(600),
-  public_transit VARCHAR(600)
+  parking_details VARCHAR(900),
+  public_transit VARCHAR(900)
 );
 
 CREATE TABLE IF NOT EXISTS tags(
@@ -31,7 +33,7 @@ CREATE TABLE IF NOT EXISTS tags(
   tag_name VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXISTS tags_restaurant(
+CREATE TABLE IF NOT EXISTS restaurants_tags(
   id SERIAL PRIMARY KEY,
   restaurant_id INT REFERENCES overviews(rest_id),
   tag_id INT REFERENCES tags(id),
