f055c3760a7a955005f22dd9cffbd073e5a3ab48
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-27 10:45:04 -0700

    Fix serving image and incorrect post REST endpoint.

--- server/application.js
+++ server/application.js
@@ -18,9 +18,9 @@ app.use(morgan('dev', {
   }))
   .use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public/index.html')))
   .use('/overviewsBundle.js', express.static(path.join(__dirname, '../public/dist/bundle.js')))
-  .use('/images/star-rating.png', express.static(path.join(__dirname, '../public/images/star-rating.png')))
+  .use('/overviews/images/', express.static(path.join(__dirname, '../public/images/')))
   .get('/overviews/restaurant/:restaurantId/overview', controller.data.getOverview)
-  .post('/overviews/restaurant', controller.data.postOverview)
+  .post('/overviews/restaurant/:restaurantId/overview', controller.data.postOverview)
   .put('/overviews/restaurant/:restaurantId/overview', controller.data.updateOverview)
   .delete('/overviews/restaurant/:restaurantId/overview', controller.data.deleteOverview);
 
