6c72ca85fbea68f964e6bb78f423fb7e0d8d21b1
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-26 17:08:47 -0700

    Add morgan dev.

--- server/application.js
+++ server/application.js
@@ -1,10 +1,22 @@
 const controller = require('./controller');
 const express = require('express');
+const logger = require('./logger');
+const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
 
-app.use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public/index.html')))
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode < 400
+    }, stream: process.stderr
+  }))
+  .use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode >= 400
+    }, stream: process.stdout
+  }))
+  .use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public/index.html')))
   .use('/overviewsBundle.js', express.static(path.join(__dirname, '../public/dist/bundle.js')))
   .use('/images/star-rating.png', express.static(path.join(__dirname, '../public/images/star-rating.png')))
   .get('/overviews/restaurant/:restaurantId/overview', controller.data.getOverview)
