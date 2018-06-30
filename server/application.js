b0fadb1f267ddf44df8ab59b0e37a8d7bee5cf75
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-30 13:56:28 -0700

    Add postTag route.

--- server/application.js
+++ server/application.js
@@ -1,27 +1,19 @@
 const controller = require('./controller');
 const express = require('express');
-const logger = require('./logger');
-const morgan = require('morgan');
+// const logger = require('./logger');
+// const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
 
-app.use(morgan('dev', {
-    skip: function (req, res) {
-        return res.statusCode < 400
-    }, stream: process.stderr
-  }))
-  .use(morgan('dev', {
-    skip: function (req, res) {
-        return res.statusCode >= 400
-    }, stream: process.stdout
-  }))
+app
   .use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public/index.html')))
   .use('/overviewsBundle.js', express.static(path.join(__dirname, '../public/dist/bundle.js')))
   .use('/overviews/images/', express.static(path.join(__dirname, '../public/images/')))
   .get('/overviews/restaurant/:restaurantId/overview', controller.data.getOverview)
-  .post('/overviews/restaurant/:restaurantId/overview', controller.data.postOverview)
+  .post('/overviews/restaurant', controller.data.postOverview)
+  .post('/overviews/restaurant/:restaurantId/tag', controller.data.postTag)
   .put('/overviews/restaurant/:restaurantId/overview', controller.data.updateOverview)
-  .delete('/overviews/restaurant/:restaurantId/overview', controller.data.deleteOverview);
+  .delete('/overviews/restaurant/:restaurantId/overview', controller.data.deleteOverview)
 
 module.exports = app;
