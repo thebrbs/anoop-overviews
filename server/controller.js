a7a45af3426ad01a4bc7d2361ea40375b84b8a68
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-30 13:57:19 -0700

    Add redis invocations and createOverview method.

--- server/controller.js
+++ server/controller.js
@@ -1,21 +1,38 @@
 // const db = require('../database/mongo');
-const db = require('../database/cassandra');
-// const db = require('../database/postgres');
+// const db = require('../database/cassandra');
+const db = require('../database/postgres');
+const redis = require('./cacheRedis');
 
 module.exports = {
   data: {
     getOverview(req, res) {
-      db.retrieveOvr(req.params.restaurantId)
-        .then(restDetails => res.json(restDetails))
-        .catch(err => res.status(500).end(err));
+      const restId = req.params.restaurantId;
+      redis.retrieveOvrCache(restId, (err, cachedRecord) => {
+        if (cachedRecord) {
+          res.status(200).send(cachedRecord);
+        } else {
+          db.retrieveOvr(restId)
+            .then(data => {
+              redis.setOvrCache(restId, JSON.stringify(data.rows[0].row_to_json), () => {
+                res.json(data.rows[0].row_to_json);
+              });
+            })
+            .catch(err => res.status(500).end(err));
+        }
+      })
     },
     postOverview(req, res) {
-      db.createOvr(req.params)
+      db.createOvr(req.query)
+        .then(() => res.sendStatus(201))
+        .catch(err => res.status(404).end(err));
+    },
+    postTag(req, res) {
+      db.createTag(req.query)
         .then(() => res.sendStatus(201))
         .catch(err => res.status(404).end(err));
     },
     updateOverview(req, res) {
-      db.updateOvr(req.params)
+      db.updateOvr(req.params.restaurantId, req.query)
         .then(() => res.sendStatus(202))
         .catch(err => res.status(500).end(err));
     },
