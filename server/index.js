bf040b71034f05fc0bfda5baec54dc747d8fe3de
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-06-30 13:57:34 -0700

    Add node clustering.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,43 @@
-require('dotenv').config();
+// require('dotenv').config();
+require('newrelic');
+// const spdy = require('spdy');
+// const fs = require('fs')
 const app = require('./application');
+
+const cluster = require( 'cluster' );
+const cCPUs   = require( 'os' ).cpus().length;
 
 const port = process.env.PORT || 3003;
 
-app.listen(port, () => console.log(`Overview module listening on port ${port}`));
+if (cluster.isMaster) {
+  for (var i = 0; i < cCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('online', function (worker) {
+    console.log('Worker ' + worker.process.pid + ' is online.');
+  });
+  cluster.on('exit', function (worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died.');
+  });
+}
+else {
+  app.listen(port, () => console.log(`Overview module listening on port ${port}`));
+}
+
+// eyes.inspect( process.env );
+// const options = {
+//   key: fs.readFileSync(__dirname + '/http2-express/server.key'),
+//   cert:  fs.readFileSync(__dirname + '/http2-express/server.crt'),
+//   rejectUnauthorized: false,
+// }
+
+// spdy
+//   .createServer(options, app)
+//   .listen(port, (error) => {
+//     if (error) {
+//       console.error(error)
+//       return process.exit(1)
+//     } else {
+//       console.log('Listening on port: ' + port + '.')
+//     }
+//   });
