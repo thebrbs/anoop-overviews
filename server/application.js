72185cad1e2f815562693260f152691fd957e05b
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-07-02 17:23:05 -0700

    Refactor for SSR.

--- server/application.js
+++ server/application.js
@@ -8,7 +8,12 @@ const app = express();
 
 app
   .use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public/index.html')))
-  .use('/overviewsBundle.js', express.static(path.join(__dirname, '../public/dist/bundle.js')))
+
+  .use('/', express.static(path.join(__dirname, '../public/dist')))
+  // .use('/overviewsBundle-server.js', express.static(path.join(__dirname, '../public/dist/bundle-server.js')))
+  // .use('/overviewsBundle-localmicro.js', express.static(path.join(__dirname, '../public/dist/bundle-localmicro.js')))
+  // .use('/styles.css', express.static(path.join(__dirname, '../public/dist/styles.css')))
+  
   .use('/overviews/images/', express.static(path.join(__dirname, '../public/images/')))
   .get('/overviews/restaurant/:restaurantId/overview', controller.data.getOverview)
   .post('/overviews/restaurant', controller.data.postOverview)
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const src_dir = path.join(__dirname, '/client/src');
 const dist_dir = path.join(__dirname, '/public/dist');
 
-module.exports = {
-  mode: 'production',
-  entry: [
-    `${src_dir}/index.jsx`,
-  ],
+const common = {
+  context: __dirname + '/client',
+};
+
+const localmicro = {
+  entry: `${src_dir}/index.jsx`,
   module : {
     rules : [
       {
@@ -14,7 +16,7 @@ module.exports = {
         include : src_dir,
         loader : "babel-loader",
         query: {
-          presets: ['react', 'es2015'],
+          presets: ['react', 'es2015', 'env'],
         }
       },
       {
@@ -38,7 +40,88 @@ module.exports = {
     ]
   },
   output: {
-    filename: 'bundle.js',
     path: dist_dir,
-  },
+    filename: 'overview-bundle-localmicro.js'
+  }
 };
+
+const client = {
+  entry: `${src_dir}/client.jsx`,
+  module : {
+    rules : [
+      {
+        test : [/\.jsx?/],
+        include : src_dir,
+        loader : "babel-loader",
+        query: {
+          presets: ['react', 'es2015', 'env'],
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 2,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: dist_dir,
+    filename: 'overview-bundle.js'
+  }
+};
+
+const server = {
+  entry: `${src_dir}/components/Overview.jsx`,
+  target: 'node',
+  module : {
+    rules : [
+      {
+        test : [/\.jsx?/],
+        include : src_dir,
+        loader : "babel-loader",
+        query: {
+          presets: ['react', 'es2015', 'env'],
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 2,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: dist_dir,
+    filename: 'overview-bundle-server.js',
+    libraryTarget: 'commonjs-module'
+  }
+};
+
+module.exports = [
+  Object.assign({}, common, localmicro),
+  Object.assign({}, common, client),
+  Object.assign({}, common, server)
+];
